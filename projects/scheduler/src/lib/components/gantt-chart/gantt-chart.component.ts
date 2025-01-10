61cf4ad85abc2950920e094622b362e674a3c39f
ermalCerhozi <ermal.cerhozi@lutech.it>
2025-01-10 10:19:51 +0100

    Implement drag and drop feature

--- projects/scheduler/src/lib/components/gantt-chart/gantt-chart.component.ts
+++ projects/scheduler/src/lib/components/gantt-chart/gantt-chart.component.ts
@@ -12,7 +12,6 @@ import * as d3 from 'd3';
 })
 export class GanttChartComponent implements AfterViewInit, OnChanges {
     @ViewChild('chart') private chartContainer!: ElementRef;
-    // @ViewChild('elemnts') private elemnts!: ElementRef;
     @ViewChild('chartVertLabels') private chartVertLabelsContainer!: ElementRef;
     
     @Input('TableRows') tableRows!: any[];
@@ -31,15 +30,16 @@ export class GanttChartComponent implements AfterViewInit, OnChanges {
 
     svg: any; //The gannt svg
     verticalLabelsSVG: any; //The table rows svg
-    dateFormat: any;  
+    dateFormat: any;
     timeScale: any;
     numOccurances!: any[]; //The number of occurrences of events for each tableRow
 
-    width: number = 1200;
+    width: number = 1400;
     height: number = 0;
     topPadding: number = 75;
-    eventHeight: number = 25; //The height of the event
-    eventsGap: number = this.eventHeight + 4; //The gap between events
+    eventHeight: number = 48;
+    gutter: number = 4;
+    eventsGap: number = this.eventHeight + this.gutter;
 
     //DRAG AND DROP
     newX = 0;
@@ -49,7 +49,7 @@ export class GanttChartComponent implements AfterViewInit, OnChanges {
     selectedElement: any;
     isDragging = false;
 
-    constructor(private el: ElementRef) { }
+    constructor() { }
 
     ngAfterViewInit() {
         this.orderEventsByRows(this.events, this.tableRows);
@@ -98,12 +98,15 @@ export class GanttChartComponent implements AfterViewInit, OnChanges {
             .attr("height", this.height)
             .attr("class", "svg");
         
-        this.dateFormat = d3.timeParse("%d-%m-%Y");
+        this.dateFormat = d3.timeParse("%d-%m-%Y %H:%M:%S");
         
         switch (this.view) {
             case 'day':
                 this.timeScale = d3.scaleTime()
-                    .domain([new Date(this.year, this.month - 1, this.day), new Date(this.year, this.month - 1, this.day + 1)])
+                    .domain([
+                        new Date(this.year, this.month - 1, this.day),
+                        new Date(this.year, this.month - 1, this.day + 1)
+                    ])
                     .range([0, this.width - 1]);
                 break;
             case 'week':
@@ -125,6 +128,7 @@ export class GanttChartComponent implements AfterViewInit, OnChanges {
         }
 
         this.makeGrid();
+        this.drawTableShadow();
         this.drawEvents();
         this.vertLabelsSvg();
         this.draggableEvents();
@@ -144,67 +148,97 @@ export class GanttChartComponent implements AfterViewInit, OnChanges {
     * when moving move the text as well
     */
     draggableEvents() {
-        const cards = Array.from(this.chartContainer.nativeElement.querySelectorAll('rect#element') as NodeListOf<SVGRectElement>);
-        cards.forEach((card: SVGRectElement) => {
-            card.addEventListener('mousedown', (e: MouseEvent) => {
-                this.selectedElement = e.target as SVGRectElement;
-                this.startX = e.clientX;
-                this.startY = e.clientY;
-                const initialX = parseFloat(this.selectedElement.getAttribute('x') || '0');
-                const initialY = parseFloat(this.selectedElement.getAttribute('y') || '0');
-    
-                const onMouseMove = (moveEvent: MouseEvent) => {
-                    if (this.selectedElement) {
-                        this.isDragging = true;
-                        const dx = moveEvent.clientX - this.startX;
-                        const dy = moveEvent.clientY - this.startY;
-                        let newX = initialX + dx;
-                        let newY = initialY + dy;
-    
-                        // Snap logic
-                        if (dy + 14.5 < 0) { // Dragged upwards
-                            newY = initialY + Math.floor(dy / 29) * 29;
-                        } else if (dy + 14.5 > 0) { // Dragged downwards
-                            newY = initialY + Math.ceil(dy / 29) * 29;
-                        }
-    
-                        // Ensure the element does not go inside the top padding
-                        if (newY < this.topPadding) {
-                            newY = this.topPadding;
-                        }
-    
-                        // Ensure the element does not go below the end of the chartContainer
-                        const chartHeight = this.chartContainer.nativeElement.getBoundingClientRect().height;
-                        const elementHeight = this.selectedElement.getBoundingClientRect().height;
-                        if (newY + elementHeight > chartHeight ) {
-                            newY = chartHeight - elementHeight - 8;
-                        }
+        const eventGroups = this.svg.selectAll('g.event-group');
+        const self = this;
 
-                        // Ensure the element does not go outside the chart boundaries
-                        const chartWidth = this.chartContainer.nativeElement.getBoundingClientRect().width;
-                        const elementWidth = this.selectedElement.getBoundingClientRect().width;
-                        if (newX + elementWidth > chartWidth) {
-                            newX = chartWidth - elementWidth;
-                        }
-                        if (newX < 0) {
-                            newX = 0;
-                        }
+        eventGroups.on('mousedown', (event: MouseEvent, d: any) => {
+            self.selectedElement = d3.select(event.currentTarget as HTMLElement);
+            self.startX = event.clientX;
+            self.startY = event.clientY;
 
-                        this.selectedElement.setAttribute('x', newX.toString());
-                        this.selectedElement.setAttribute('y', newY.toString());
-                    }
-                };
-    
-                const onMouseUp = () => {
-                    document.removeEventListener('mousemove', onMouseMove);
-                    document.removeEventListener('mouseup', onMouseUp);
-                    this.selectedElement = null;
-                };
-    
-                document.addEventListener('mousemove', onMouseMove);
-                document.addEventListener('mouseup', onMouseUp);
-            });
+            const rect = self.selectedElement.select('rect');
+            const texts = self.selectedElement.selectAll('text');
+            const initialRectX = parseFloat(rect.attr('x'));
+            const initialRectY = parseFloat(rect.attr('y'));
+
+            const onMouseMove = function(moveEvent: MouseEvent) {
+                if (self.selectedElement) {
+                    self.isDragging = true;
+                    const dx = moveEvent.clientX - self.startX;
+
+                    let newRectX = initialRectX + dx;
+
+                    // Horizontal boundaries
+                    const chartWidth = self.width;
+                    const rectWidth = parseFloat(rect.attr('width'));
+                    if (newRectX < 0) newRectX = 0;
+                    if (newRectX + rectWidth > chartWidth) newRectX = chartWidth - rectWidth;
+
+                    // Update rect position
+                    rect.attr('x', newRectX);
+
+                    self.selectedElement.select('.full-line').attr('x', newRectX);
+
+                    self.selectedElement.select('.progress-line').attr('x', newRectX);
+
+                    // Update texts position relative to rect
+                    texts.attr('x', newRectX + 8)
+                         .attr('y', (d: any, i: number) => {
+                             return initialRectY + (i === 0 ? 20 : 35); // Keep Y position unchanged
+                         });
+                }
+            };
+
+            const onMouseUp = function() {
+                if (self.isDragging && self.selectedElement) {
+                    const rect = self.selectedElement.select('rect');
+                    const newX = parseFloat(rect.attr('x'));
+                    const rectWidth = parseFloat(rect.attr('width'));
+
+                    const newStartDateTime = self.timeScale.invert(newX);
+                    const newEndDateTime = self.timeScale.invert(newX + rectWidth);
+
+                    d.startDate = d3.timeFormat("%d-%m-%Y")(newStartDateTime);
+                    d.startTime = d3.timeFormat("%H:%M:%S")(newStartDateTime);
+                    d.endDate = d3.timeFormat("%d-%m-%Y")(newEndDateTime);
+                    d.endTime = d3.timeFormat("%H:%M:%S")(newEndDateTime);
+                    console.log(d);
+
+                    self.updateEventPosition(self.selectedElement, d);
+                }
+
+                document.removeEventListener('mousemove', onMouseMove);
+                document.removeEventListener('mouseup', onMouseUp);
+                self.selectedElement = null;
+                self.isDragging = false;
+            };
+
+            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mouseup', onMouseUp);
         });
+    }
+
+    updateEventPosition(eventGroup: any, eventData: any) {
+        const rect = eventGroup.select('rect#element');
+        const fullLine = eventGroup.select('.full-line');
+        const progressLine = eventGroup.select('.progress-line');
+        const titleText = eventGroup.select('text:nth-of-type(1)');
+        const detailsText = eventGroup.select('text:nth-of-type(2)');
+
+        const newX = this.timeScale(this.dateFormat(`${eventData.startDate} ${eventData.startTime}`));
+        const newWidth = this.timeScale(this.dateFormat(`${eventData.endDate} ${eventData.endTime}`)) - newX;
+
+        rect.attr('x', newX)
+            .attr('width', newWidth);
+
+        fullLine.attr('x', newX)
+            .attr('width', newWidth);
+
+        progressLine.attr('x', newX)
+            .attr('width', newWidth * (eventData.done / 100));
+
+        titleText.attr('x', newX + 8);
+        detailsText.attr('x', newX + 8);
     }
 
     resizableEvents() { 
@@ -214,7 +248,7 @@ export class GanttChartComponent implements AfterViewInit, OnChanges {
     makeGrid() {
         var xAxis = d3.axisTop(this.timeScale)
             .ticks(d3.timeDay)
-            .tickSize(-this.height + (-this.topPadding) )
+            .tickSize(-this.height + this.topPadding)
             .tickSizeOuter(0)
 
             if (this.view === 'day') {
@@ -227,7 +261,7 @@ export class GanttChartComponent implements AfterViewInit, OnChanges {
     
         this.svg.append('g')
             .attr('class', 'grid')
-            .attr('transform', 'translate(' + 0 + ', ' + (this.height - 320) + ')')
+            .attr('transform', 'translate(0, ' + (this.topPadding - this.gutter/2) + ')')
             .call(xAxis)
             .selectAll("text")
             .style("text-anchor", "middle")
@@ -236,79 +270,122 @@ export class GanttChartComponent implements AfterViewInit, OnChanges {
             .attr("dx", "2em");
     }
 
-    drawEvents() {
+    drawTableShadow() {
         this.svg.append("g")
-            .selectAll("rect")
-            .data(this.events)
-            .enter()
-            .append("rect")
-            .attr("x", 0)
-            .attr("y", (d: any, i: any) => {
-                return i * this.eventsGap + this.topPadding;
-            })
-            .attr("width", (d: any) => {
-                return this.width;
-            })
-            .attr("height", this.eventsGap)
-            .attr("stroke", "none")
-            .attr("fill", (d: any) => {
-                for (var i = 0; i < this.tableRows.length; i++) {
-                    if (d.rowId == this.tableRows[i].id) {
-                        if( i%2 == 0 ){
-                            return '#BDBDBD';
-                        }
+        .selectAll("rect")
+        .data(this.events)
+        .enter()
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", (d: any, i: any) => {
+            return i * this.eventsGap + this.topPadding -2;
+        })
+        .attr("width", (d: any) => {
+            return this.width;
+        })
+        .attr("height", this.eventsGap)
+        .attr("stroke", "none")
+        .attr("fill", (d: any) => {
+            for (var i = 0; i < this.tableRows.length; i++) {
+                if (d.rowId == this.tableRows[i].id) {
+                    if( i%2 == 0 ){
+                        return '#BDBDBD';
                     }
                 }
-                return '#000';
-            })
-            .attr("opacity", 0.2);
+            }
+            return '#000';
+        })
+        .attr("opacity", 0.2);
+    }
 
-        var rectangles = this.svg.append('g')
-            .selectAll("rect")
+    drawEvents() {
+        var eventGroups = this.svg.append('g')
+            .selectAll('g')
             .data(this.events)
-            .enter();
+            .enter()
+            .append('g')
+            .attr('class', 'event-group')
+            .style('cursor', 'pointer');
 
-        var innerRects = rectangles.append("rect")
+        eventGroups.append('rect')
             .attr("id", "element")
-            .attr("rx", 14)
-            .attr("ry", 500)
             .attr("x", (d: any) => {        
-                return this.timeScale(this.dateFormat(d.startDate));
+                return this.timeScale(this.dateFormat(`${d.startDate} ${d.startTime}`));
             })
             .attr("y", (d: any, i: any) => {
                 return i * this.eventsGap + this.topPadding;
             })
             .attr("width", (d: any) => {
-                return (this.timeScale(this.dateFormat(d.endDate)) - this.timeScale(this.dateFormat(d.startDate)));
+                return (this.timeScale(this.dateFormat(`${d.endDate} ${d.endTime}`)) - this.timeScale(this.dateFormat(`${d.startDate} ${d.startTime}`)));
             })
             .attr("height", this.eventHeight)
             .attr("stroke", "none")
             .attr("fill", (d: any) => {
                 for (var i = 0; i < this.tableRows.length; i++) {
                     if (d.rowId == this.tableRows[i].id) {
-                        return d.status;
+                        return d.color;
                     }
                 }
             })
-            .style("cursor", "pointer")
-            .style("position", "fixed");
+            .style("cursor", "pointer");
 
-        rectangles.append("text")
-            .text((d: any) => {
-                return d.title;
-            })
+        eventGroups.append('rect')
+            .attr("class", "full-line")
             .attr("x", (d: any) => {
-                return (this.timeScale(this.dateFormat(d.endDate)) - this.timeScale(this.dateFormat(d.startDate))) / 2 + this.timeScale(this.dateFormat(d.startDate)); 
+                return this.timeScale(this.dateFormat(`${d.startDate} ${d.startTime}`));
             })
             .attr("y", (d: any, i: any) => {
-                return i * this.eventsGap + 14 + this.topPadding;
+                return i * this.eventsGap + this.topPadding;
             })
-            .attr("font-size", 11)
-            .attr("text-anchor", "middle")
-            .attr("text-height", this.eventHeight)
-            .attr("fill", "#fff");
+            .attr("width", (d: any) => {
+                return this.timeScale(this.dateFormat(`${d.endDate} ${d.endTime}`)) - this.timeScale(this.dateFormat(`${d.startDate} ${d.startTime}`));
+            })
+            .attr("height", 7)
+            .attr("fill", "#FFFFFF");
 
-        innerRects.on('click', (data: any) => {
+        eventGroups.append('rect')
+            .attr("class", "progress-line")
+            .attr("x", (d: any) => {
+                return this.timeScale(this.dateFormat(`${d.startDate} ${d.startTime}`));
+            })
+            .attr("y", (d: any, i: any) => {
+                return i * this.eventsGap + this.topPadding;
+            })
+            .attr("width", (d: any) => {
+                const eventWidth = this.timeScale(this.dateFormat(`${d.endDate} ${d.endTime}`)) - this.timeScale(this.dateFormat(`${d.startDate} ${d.startTime}`));
+                return eventWidth * (d.done / 100);
+            })
+            .attr("height", 5)
+            .attr("fill", "#FF0000");
+
+        eventGroups.append("text")
+            .text((d: any) => d.title.toUpperCase())
+            .attr("x", (d: any) => {
+                return this.timeScale(this.dateFormat(`${d.startDate} ${d.startTime}`)) + 8;
+            })
+            .attr("y", (d: any, i: any) => {
+                return i * this.eventsGap + this.topPadding + 20;
+            })
+            .attr("font-size", 14)
+            .attr("text-anchor", "start")
+            .attr("fill", "#000")
+
+        eventGroups.append("text")
+            .text((d: any) => {
+                const startTime = d.startTime.split(':').slice(0, 2).join(':');
+                const endTime = d.endTime.split(':').slice(0, 2).join(':');
+                return d.done + '%' + '\u00A0\u00A0⬤\u00A0\u00A0' + `${startTime} - ${endTime}`;            })
+            .attr("x", (d: any) => {
+                return this.timeScale(this.dateFormat(`${d.startDate} ${d.startTime}`)) + 8;
+            })
+            .attr("y", (d: any, i: any) => {
+                return i * this.eventsGap + this.topPadding + 35;
+            })
+            .attr("font-size", 12)
+            .attr("text-anchor", "start")
+            .attr("fill", "#555")
+
+        eventGroups.on('click', (data: any) => {
             if (!this.isDragging) {
                 this.eventClicked.emit(data.srcElement.__data__);
             }
@@ -385,6 +462,5 @@ export class GanttChartComponent implements AfterViewInit, OnChanges {
     }
 
     calculateHeight(): void {
-        this.height = this.topPadding + this.events.length * 29;
-    }
-}
+        this.height = this.topPadding + this.events.length * (this.eventsGap);
+    }}
